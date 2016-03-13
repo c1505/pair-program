1b2326fd51bd0a225cbab8029eb082860d44d84f
c1505 <coreym247@gmail.com>
2016-03-12 20:29:23 -0500

    added devise and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
+  # resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :welcome, only: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/migrate/20160306230708_create_users.rb
+++ db/migrate/20160306230708_create_users.rb
@@ -1,16 +1,16 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
+class CreateUsers <ActiveRecord::Migration    
+  def change            
     create_table :users do |t|
       t.string :name
-      t.string :email
       t.string :slack
       t.string :github
       t.string :twitter
       t.string :website
       t.text :bio
+      t.text :availability
       t.string :application
 
       t.timestamps null: false
-    end
-  end
+     end
+   end
 end
