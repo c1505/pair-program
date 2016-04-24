996433a8fc2d20301aaad9cce19c5eb2b2f060c7
c1505 <coreym247@gmail.com>
2016-04-24 12:58:53 -0400

    edited initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,6 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  # config.omniauth :facebook, "1505363863106421", "5afef75189e24cfb32a36566341580e5"
   config.omniauth :facebook, "1505363863106421", Rails.application.secrets.facebook_secret
 
 
