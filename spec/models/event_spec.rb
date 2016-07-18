08f1f15edccc07dd4cc1e94585e3fd2d443676c0
c1505 <coreym247@gmail.com>
2016-07-17 23:19:10 -0400

    validation tests started

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Event do
+  it "is valid with start dates"
+    # event = Event.new(earliest_start:    , latest_start:  )
+  it "is invalid without start dates"
+
+  it "is valid with a repo link"
+  it "is invalid without a repo link"
+
+  it "is valid with a host"
+  it "is invalid without a host"
+
+end
+
+
+  # it "is valid with a title if exercism"
